fdb76eee2cfe5a7536733bfc624cb97e1bcaaf1f
kchhananu-creator <kchhananu@gmail.com>
2025-11-27 18:43:38 +0530

    my 2nd task completed

---
+++ Upadte query.sql
@@ -0,0 +1,218 @@
+select * from course
+update
+
+update course set course_fee = 20000 where course_id = 6 
+
+select * from course
+
+update course set start_date = '2024-09-21', course_name = 'AI' where course_id = 3
+
+select * from course
+
+update course set is_active = 'false'
+
+select * from course 
+
+update course set course_name = 'RM' where course_id = 1 or course_id = 2
+
+select * from course
+
+select * from course where course_fee = 9000
+
+update course set duration_months = 4 where course_fee = 9000
+
+update course set course_name = 'advanced c programming' where course_name = 'c programming'
+
+select * from course 
+
+update course set duration_months = 3 where course_fee = 5500
+
+select * from course
+
+update course set start_date = '2024-07-01' where course_name = 'android development'
+
+select * from course
+
+update course set course_fee = 8500 where course_id = 1
+
+select * from course
+
+update course set course_name = 'python intermediate' where course_id = 2
+
+select * from course
+
+select * from doctor
+
+update doctor set fees = 900 where doctor_id = 1
+
+select * from doctor
+
+update doctor set specialist = 'skin specialist' where doctor_id = 2
+
+select * from doctor
+
+update doctor set experience = 12 where doctor_id = 3
+
+select * from doctor
+
+update doctor set city = 'mumbai' where doctor_id = 4
+
+select * from doctor
+
+update doctor set fees = 1100 where doctor_id = 5
+
+select * from doctor
+
+
+update doctor set name = 'neha s.' where specialist = 'dentist
+
+select * from doctor
+
+update doctor set city = 'pune' where name = 'amit deshmukh'
+
+select * from doctor
+
+update doctor set fees = 600 where city = 'nagpur'
+
+select * from doctor
+
+update doctor set experience = 5 where specialist = 'psychiatrist'
+
+select * from doctor
+
+update doctor set specialist = 'eye expert' where name = 'riya pandey'
+
+select * from doctor
+
+select * from hostel
+
+update hostel set fees = 26000 where hostel_id = 1
+
+select * from hostel
+
+update hostel set capacity = 5 where hostel_id = 2
+
+select * from hostel
+
+update hostel set room_no = 110 where hostel_id = 3
+
+select * from hostel
+
+update hostel set warden_name = 'mr sharma ji' where hostel_id = 4
+
+select * from hostel
+
+update hostel set hostel_name = 'green stay' where hostel_id = 5
+
+select * from hostel
+
+update hostel set fees = 32000 where hostel_name = 'blue hostel'
+
+select * from hostel
+
+update hostel set capacity = 4 where room_no = 203
+
+select * from hostel
+
+update hostel set room_no = 210 where warden_name = 'mr patil'
+
+select * from hostel
+
+update hostel set hostel_name = 'ladies premium hostel' where hostel_id = 16
+
+select * from hostel
+
+update hostel set fees = 25000 where hostel_name = 'boys hostel'
+
+select * from hostel
+
+select * from medicine
+
+update medicine set price = 22 where medicine_id = 1
+
+select * from medicine
+
+update medicine set stock = 175 where medicine_id = 2
+
+select * from medicine
+
+update medicine set price = 65 where medicine_id = 3
+
+select * from medicine
+
+update medicine set stock = 230 where medicine_id = 4
+
+select * from medicine
+
+update medicine set manufacturer = 'care pharma' where medicine_id = 5
+
+select * from medicine
+
+update medicine set price = 45 where medicine_id = 6
+
+select * from medicine
+
+update medicine set stock = 210 where medicine_id = 7
+
+select * from medicine
+
+update medicine set price = 50 where medicine_id = 8
+
+select * from medicine
+
+update medicine set stock = 280 where medicine_id = 9
+
+select * from medicine
+
+update medicine set price = 28 where medicine_id = 10
+
+select * from medicine
+
+
+select * from friends
+
+update friends set hobby = 'music' where friend_id = 1
+
+select * from friends
+
+update friends set insta_id = 'riya_artist' where friend_id = 2
+
+select * from friends
+
+update friends set contact_no = '9876543999' where friend_id = 3
+
+select * from friends
+
+update friends set name = 'pooja kumari' where friend_id = 4
+
+select * from friends
+
+update friends set hobby = 'basketball' where friend_id = 5
+
+select * from friends
+
+update friends set insta_id = 'meera_voice' where friend_id = 6
+
+select * from friends
+
+update friends set contact_no = '9876543888' where friend_id = 7
+
+select * from friends
+
+update friends set name = 'anjali sharma' where friend_id = 8
+
+select * from friends
+
+update friends set hobby = 'yoga' where friend_id = 9
+
+select * from friends
+
+update friends set insta_id = 'simran_glowup' where friend_id = 10
+
+select * from friends
+
+
+
+
+
+
